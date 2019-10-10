70f7152c0f9387299c58ddc185bf4c3adb6d5aaf
Jonathan <jonathanholson@gmail.com>
2019-10-10 09:54:24 -0700

    User Can Now Choose Song From Dropdown
    - Note: user has to pause current song before choosing new song

--- src/app.jsx
+++ src/app.jsx
@@ -2,18 +2,43 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentSong: null,
+      currentSong: new Audio('./Assets/song.mp3'),
       currentSongCurrentTime: 0,
+      songs: [
+        './Assets/song.mp3',
+        './Assets/All_I_got.mp3',
+        './Assets/Say_My_Name.mp3',
+      ],
+      currentSongIndex: 0,
     };
 
     this.changeCurrentSong = this.changeCurrentSong.bind(this);
     this.playSong = this.playSong.bind(this);
     this.pauseSong = this.pauseSong.bind(this);
+    this.handleSongChoice = this.handleSongChoice.bind(this);
   }
 
-  changeCurrentSong(songAddress = './song.mp3') {
+  changeCurrentSong() {
+    // Get next song's index
+    const nextSongIndex =
+      (this.state.currentSongIndex + 1) % this.state.songs.length;
+    // Create audio element given path from state
+    console.log(`next song url: ${this.state.songs[nextSongIndex]}`);
+    const nextSong = new Audio(this.state.songs[nextSongIndex]);
+    // Set new song as current song in state
     this.setState({
-      currentSong: new Audio(songAddress),
+      currentSong: nextSong,
+      currentSongIndex: nextSongIndex,
+    });
+  }
+
+  handleSongChoice(event) {
+    this.setState({currentSong: new Audio(event.target.value)});
+  }
+
+  chooseCurrentSong(songURL) {
+    this.setState({
+      currentSong: new Audio(songURL),
     });
   }
 
@@ -31,11 +56,21 @@ class App extends React.Component {
   }
 
   render() {
+    const {songs} = this.state;
     return (
       <div id='playbackCenter'>
-        <button id='chooseSong' onClick={this.changeCurrentSong}>
+        {/* <button id='chooseSong' onClick={this.changeCurrentSong}>
           Change Song
-        </button>
+        </button> */}
+        <select
+          name='song-select'
+          id='song-select'
+          onChange={this.handleSongChoice}
+        >
+          <option value={songs[0]}>Flicker</option>
+          <option value={songs[1]}>All I Got</option>
+          <option value={songs[2]}>Say My Name</option>
+        </select>
         <button id='play' onClick={this.playSong}>
           Play
         </button>
