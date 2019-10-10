5cbb97f15d027b8dfa1653f350f5168e6e845c11
Jonathan <jonathanholson@gmail.com>
2019-10-10 12:15:47 -0700

    Implemented Integer Playback Counter
    - Counter resides in state
    - Calls to 'Play Song' start the timer, which is setting an interval that runs an 'Increment Timer' method
    - The ID for the interval is stored in state
    - Calls to 'Pause Song' clear the increment interval by accessing the interval ID stored in state

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    // Set state - mostly revolves around current song playing
     this.state = {
       currentSong: new Audio('./Assets/song.mp3'),
       currentSongCurrentTime: 0,
@@ -10,12 +12,19 @@ class App extends React.Component {
         './Assets/Say_My_Name.mp3',
       ],
       currentSongIndex: 0,
+      currentTime: 0,
+      timerIntervalID: null,
     };
 
+    // Bind functions to this
     this.changeCurrentSong = this.changeCurrentSong.bind(this);
+    this.handleSongChoice = this.handleSongChoice.bind(this);
+    this.chooseCurrentSong = this.chooseCurrentSong.bind(this);
     this.playSong = this.playSong.bind(this);
     this.pauseSong = this.pauseSong.bind(this);
-    this.handleSongChoice = this.handleSongChoice.bind(this);
+    this.incrementTimer = this.incrementTimer.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
   }
 
   changeCurrentSong() {
@@ -43,16 +52,43 @@ class App extends React.Component {
   }
 
   playSong() {
-    console.log('play pressed');
+    // Start song playback
     if (this.state.currentSong) {
       this.state.currentSong.play();
+      // Start timer
+      this.startTimer();
     }
   }
 
   pauseSong() {
     if (this.state.currentSong) {
       this.state.currentSong.pause();
+      // Stop timer
+      this.stopTimer();
     }
+  }
+
+  incrementTimer() {
+    const currentTime = this.state.currentSong.currentTime;
+    this.setState({
+      currentTime: Math.floor(currentTime + 1),
+    });
+  }
+
+  startTimer() {
+    // Update timer every second
+    const timerIntervalID = setInterval(this.incrementTimer, 1000);
+    // Record id of interval
+    this.setState({
+      timerIntervalID,
+    });
+  }
+
+  stopTimer() {
+    // Get ID of timer currently running
+    const ID = this.state.timerIntervalID;
+    // Clear interval with id
+    clearInterval(ID);
   }
 
   render() {
@@ -77,6 +113,9 @@ class App extends React.Component {
         <button id='pause' onClick={this.pauseSong}>
           Pause
         </button>
+        <div id='current-playback-time'>
+          Current Playback time: {this.state.currentTime}
+        </div>
       </div>
     );
   }
