2e4c891f275ab388d66504c8ff2d95764279d941
Jonathan <jonathanholson@gmail.com>
2019-10-11 15:51:28 -0700

    Re-Configured Webpack; Added HTML Serving Over Express; App Fully Functional

--- songDisplayComponent/Server/server.js
+++ songDisplayComponent/Server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Sidebar is on port 5000; use 5001
 const port = 5001;
+
+// Serve the static index file from the React app
+app.use(express.static(path.join(__dirname, '../public/')));
 
 app.get('/', (req, res) => {
   res.end('Get / WORKS');
