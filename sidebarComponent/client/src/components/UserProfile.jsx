361eab2f671f26d23984109e127fe2e614ad51b3
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-10-18 12:50:00 -0700

    Remove borders from icons caused by not setting img src

--- sidebarComponent/client/src/components/UserProfile.jsx
+++ sidebarComponent/client/src/components/UserProfile.jsx
@@ -6,7 +6,10 @@ let UserProfile = function(props) {
       <img className={style.art} src="./assets/pictures/sample.jpg" />
       <div className={style.username}>{props.user.username}</div>
       <div className={style.followers}>
-        <img className={style['followers-icon']} />
+        <img
+          className={style['followers-icon']}
+          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyOCIgaGVpZ2h0PSIyOCI+PHBhdGggZmlsbD0icmdiYSgxNTMsIDE1MywgMTUzLCAwLjcpIiBkPSJNMTguNCAxOC41bDIuNSA1IC4yLjVIMjhsLTIuMS00LjMtNC4xLTEuNXYtMi41YzEuMi0xLjEgMS44LTMuMiAxLjgtNS4xIDAtMi4xLTItMy42LTMuNS0zLjZzLTMuNSAxLjYtMy41IDMuNmMwIDEuOS41IDQgMS44IDUuMXYyLjVoLS4xbC4xLjN6Ii8+PHBhdGggZmlsbD0iIzk5OSIgZD0iTTE3LjUgMTlsLTUtMS44di0zYzEuNC0xLjIgMi0zLjggMi01LjkgMC0yLjQtMi4zLTQuMy00LTQuMy0xLjcgMC00IDEuOC00IDQuMyAwIDIuMi42IDQuNyAyIDUuOXYzbC01IDEuOEwxIDI0aDE5bC0yLjUtNXoiLz48L3N2Zz4="
+        />
         <span className={style['follower-count']}>
           {props.user.follower_count}
         </span>
