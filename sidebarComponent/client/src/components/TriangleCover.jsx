b4738b478b40e8ae361341c51e0b00d41177b42a
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-10-21 11:52:15 -0700

    Consolidate all triangle related components and MouseBridge into Triangle.jsx. Consolidate all respective styling into Triangle.css

--- sidebarComponent/client/src/components/TriangleCover.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './TriangleCover.css';
-
-let TriangleCover = function(props) {
-  return <div className={style.triangle}></div>;
-};
-
-export default TriangleCover;
--- sidebarComponent/client/src/sidebar.test.js
+++ sidebarComponent/client/src/sidebar.test.js
@@ -1,7 +1,10 @@
-import { shallow } from 'enzyme';
-import App from './components/App.jsx';
 import React from 'react';
-import '@babel/polyfill';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+import { shallow } from 'enzyme';
+import App from '../client/App';
+
+configure({ adapter: new Adapter() });
 
 describe('Related Tracks', () => {
   it('renders three <Song /> components', () => {
