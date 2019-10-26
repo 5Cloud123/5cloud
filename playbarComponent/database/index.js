726644d80744e57fb344d951cd8e3e06afb07596
Jonathan Olson <41136100+MyNameIsJonathan@users.noreply.github.com>
2019-10-26 10:57:09 -0700

    Update index.js

--- playbarComponent/database/index.js
+++ playbarComponent/database/index.js
@@ -1,10 +1,9 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'mysql',
   user: 'root',
-  password: process.env.HR_FRONTEND_MYSQL_PASSWORD,
-  database: 'musicDB',
+  password: '',
+  database: 'musicDB'
 });
 
 connection.connect(() => console.log('DB is connected!'));
