9db0aacb3c4f1a98085b73063ff38c6a5bc8ea14
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-10-26 10:56:12 -0700

    Add cors middleware

--- sidebarComponent/server/index.js
+++ sidebarComponent/server/index.js
@@ -1,13 +1,14 @@
 let express = require('express');
 let db = require('../database/methods.js');
 let path = require('path');
+let cors = require('cors');
 
 let app = express();
 
 const PORT = 5000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/:songid', express.static(path.join(__dirname, '../public/dist')));
 
 app.use(function(req, res, next) {
