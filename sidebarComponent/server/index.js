1c48da724fb7a692917a24374ba66aaf0d0df854
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-10-26 11:55:22 -0700

    Move cors middleware before json parser in server

--- sidebarComponent/server/index.js
+++ sidebarComponent/server/index.js
@@ -7,16 +7,16 @@ let app = express();
 
 const PORT = 5000;
 
-app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(express.json());
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 app.use('/:songid', express.static(path.join(__dirname, '../public/dist')));
 
 app.get('/currentSong/:songid', (req, res) => {
