373e20f6180ff95caba374890ea8bb435b1a15b4
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-10-26 11:19:03 -0700

    Move cors middleware above express static service

--- sidebarComponent/server/index.js
+++ sidebarComponent/server/index.js
@@ -9,8 +9,6 @@ const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/:songid', express.static(path.join(__dirname, '../public/dist')));
-
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header(
@@ -19,6 +17,7 @@ app.use(function(req, res, next) {
   );
   next();
 });
+app.use('/:songid', express.static(path.join(__dirname, '../public/dist')));
 
 app.get('/currentSong/:songid', (req, res) => {
   console.log('being hit with GET req for current song info');
